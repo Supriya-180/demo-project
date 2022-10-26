c66e819449079024ae2adfe55b8f221776c505d5
supriya-180 <supriyagkp206@gmail.com>
2022-10-26 13:46:39 +0530

    wishlist_controller

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,6 +1,8 @@
 class WishlistsController < ApplicationController
 
 	def index
+
+		
 		#@wishlists = current_user.likes
 		wishlists=current_user.likes.where(likeable_type: "Product").pluck(:likeable_id).uniq
 		@products = Product.where(id: wishlists)
@@ -10,7 +12,7 @@ class WishlistsController < ApplicationController
 
     def destroy
     	
-    	
+
     	@like = current_user.likes.find_by(likeable_id: params[:id],likeable_type: "Product")
     	@like.destroy
     	redirect_to home_index_path
