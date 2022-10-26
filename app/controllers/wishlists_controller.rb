8036b648f55e558cde13e8e51c368eab1bfa9cb7
supriya-180 <supriyagkp206@gmail.com>
2022-10-26 12:43:21 +0530

    change in destoy method

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -9,7 +9,8 @@ class WishlistsController < ApplicationController
 	end
 
     def destroy
-    	# byebug
+    	
+    	
     	@like = current_user.likes.find_by(likeable_id: params[:id],likeable_type: "Product")
     	@like.destroy
     	redirect_to home_index_path
